656e20ed639a23f4723317e77f7ae1881fca8b76
rakaaufar18 <60309822+rakaaufar18@users.noreply.github.com>
2020-07-03 18:39:19 +0700

    Add files via upload

---
+++ FM 25 script-17523167.R
@@ -0,0 +1,15 @@
+# Praktikum FM 25
+# Nama  : Raka Aufar Luthfan  
+# NIM   : 17523167
+
+# Soal A 
+# Melakukan Import FMA_calonsarjana.xls
+FMA_calonsarjana <- read_excel("C:/1KULIAH/.SEMESTER 6/funmat/KELOMPOK R STUDIO/FMA_calonsarjana.xlsx")
+#Menampilkan tabel FMA_calonsarjana.xls
+View(FMA_calonsarjana)
+
+# Soal B
+# Melakukan Import FMB_calonsarjana.xls
+FMB_calonsarjana <- read_excel("C:/1KULIAH/.SEMESTER 6/funmat/KELOMPOK R STUDIO/FMB_calonsarjana.xlsx")
+#Menampilkan tabel FMB_calonsarjana.xls
+View(FMB_calonsarjana)
---
+++ FM 26 script-17523167.R
@@ -0,0 +1,12 @@
+# Praktikum FM 26
+# Nama  : Raka Aufar Luthfan
+# NIM   : 17523167
+
+# Melakukan install package "freqdist"
+install.packages("freqdist")
+
+# Melakukan pengecekan library freqdist
+library("freqdist")
+
+# Mencari Frekuensi dan persentase kumulatif data kolom ke 5
+freqdist(FMA_calonsarjana$K5)
